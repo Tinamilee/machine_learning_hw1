bf530e4562ac21b39ee6404101f280f62bf14ba1
Ching-Ping Sun <tomjpsun@gmail.com>
2017-10-25 13:20:55 +0800

    add play_ground module to test derivatives

--- gradient.py
+++ gradient.py
@@ -1,7 +1,21 @@
 # -*- coding: utf8 -*-
 import pandas as pd
 
-fname = "/Users/tom/Downloads/train.csv"
-data = pd.read_csv(fname, encoding = 'utf-8')
-print(type(data))
-print(data)
+class Measure:
+    def __init__(self):
+        file = "/Users/tom/Downloads/train.csv"
+        df = pd.read_csv(file, encoding='utf-8')
+        self.data_frame = df
+        print("shape = {}".format(df.shape))
+        print(df.iloc[0:4, 0:6])
+
+    def get_measured_data(self, row, hour = 0):
+        return self.data_frame.iloc[row, 3+hour:]
+
+
+
+measure = Measure()
+d1 = measure.get_measured_data(0)
+print(d1)
+d2 = measure.get_measured_data(1)
+print(d2)
---
+++ play_ground.py
@@ -0,0 +1,31 @@
+import functools as ft
+import matplotlib.pyplot as plt
+import numpy as np
+
+def square(x):
+    return x * x
+
+def difference_quotient(f, x, h):
+    return (f(x + h) - f(x)) / h
+
+def derivative(x):
+    return 2 * x
+
+def partial_difference_quotient(f, v, i, h):
+    """計算 f 在 v 中第 i 個元素所對應的差商"""
+    w = [v_j + (h if j == i else 0) for j, v_j in enumerate(v)]
+    return (f(w) - f(v)) / h
+
+def estimate_gradient(f, v, h=0.00001):
+    return [partial_difference_quotient(f, v, i, h) for i, _ in enumerate(v)]
+
+
+derivative_estimate = ft.partial(difference_quotient, square, h=0.0001)
+
+x = range(-10, 10)
+
+plt.title("Actual derivatives vs. Estimates")
+plt.plot(x, list(map(derivative, x)), 'rx')
+plt.plot(x, list(map(derivative_estimate, x)), 'b+')
+plt.legend(loc = 9)
+plt.show()
