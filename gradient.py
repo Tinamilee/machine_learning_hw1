a9dd032f49a71b5e9e0d5721153432d75bb16a00
Ching-Ping Sun <tomjpsun@gmail.com>
2017-10-27 17:36:37 +0800

    prepare to purify data

--- gradient.py
+++ gradient.py
@@ -2,20 +2,29 @@
 import pandas as pd
 
 class Measure:
+    Hour = 3
+    Date = 0
+    ItemName = 2
+    TotalItems = 18
     def __init__(self):
         file = "/Users/tom/Downloads/train.csv"
         df = pd.read_csv(file, encoding='utf-8')
         self.data_frame = df
         print("shape = {}".format(df.shape))
-        print(df.iloc[0:4, 0:6])
+        print(df.iloc[0:Measure.TotalItems, 0:3])
+        print('----')
+        print ("type = {}".format(type(df.iloc[:,0])))
+        print('----')
 
-    def get_measured_data(self, row, hour = 0):
-        return self.data_frame.iloc[row, 3+hour:]
+    def purify_data(self):
+        """convert RAINFALL NR to 0"""
+        i,j = self.data_frame.shape
 
+    def get_data(self, row, start=0, end=27):
+        return self.data_frame.iloc[row, Measure.Hour+start:Measure.Hour+end]
 
 
 measure = Measure()
-d1 = measure.get_measured_data(0)
-print(d1)
-d2 = measure.get_measured_data(1)
-print(d2)
+d1 = measure.get_data(0)
+measure.purify_data()
+
